339fea805ab613a8b7b692a2d0cecc1a5093f79f
Jonas Sitzmann <jonasitzmann@github.com>
2019-11-05 09:42:46 +0100

    fix logging of dicts

--- run.py
+++ run.py
@@ -1,2 +1,6 @@
+import os
+os.system('bot "installing packages from requirements.txt"')
+os.system('pip install -r requirements.txt')
+os.system('bot "done"')
 from spam_classification import big_run
 big_run()
--- spam_classification.py
+++ spam_classification.py
@@ -1,5 +1,6 @@
 import argparse
 import os
+import pprint
 import pickle
 import re
 import shutil
@@ -98,13 +99,13 @@ def big_run():
             'feature_selection__k': [1000, 2000, 4000, 8000],
             'classifier__hidden_layer_sizes': [(20, 20, 20), (10, 40, 10), (5, 10, 5), (20, 30, 30, 20)]
         }
-        log('params:\n{}'.format(hyperparams))
+        log('params:\n{}'.format(pprint.pformat(hyperparams)))
         gs_classifier = GridSearchCV(
             pipeline, hyperparams, n_jobs=-1, verbose=args.verbose, cv=2, scoring='balanced_accuracy')
         xs_train, xs_test, ys_train, ys_test = train_test_split(xs, ys)
         gs_classifier = gs_classifier.fit(xs_train, ys_train)
         acc = gs_classifier.score(xs_test, ys_test)
-        log('best params:\n{}'.format(gs_classifier.best_params_))
+        log('best params:\n{}'.format(pprint.pformat(gs_classifier.best_params_)))
         log('accuracy: {}'.format(acc))
     except Exception as ex:
         log('\nError:\n{}'.format(str(ex)))
