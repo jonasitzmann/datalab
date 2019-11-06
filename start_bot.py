4f5c505b9d6eae1eeb6b23a3e3888d3d1c7fe21c
Jonas Sitzmann <jonasitzmann@hotmail.de>
2019-11-06 11:06:02 +0100

    fix bug

--- start_bot.py
+++ start_bot.py
@@ -53,7 +53,7 @@ def echo(update, context):
         cmd_out = check_output(["git pull"], shell=True)
     elif text.lower() == 'push':
         Popen("bot 'pushing'", shell=True)
-        cmd_out = check_output(["git add -A; git commit -m 'bot commit']; git push", shell=True)
+        cmd_out = check_output(["git add -A; git commit -m 'bot commit'; git push"], shell=True)
     elif text.lower() == 'stop':
         Popen("bot 'killing all jobs'", shell=True)
         Popen("pkill -f run.py", shell=True)
