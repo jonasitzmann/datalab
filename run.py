cfe3c291d0d281f8e0ab1e94e912c858df2b3df1
Jonas Sitzmann <jonasitzmann@hotmail.de>
2019-11-07 12:16:13 +0100

    move import into try block

--- run.py
+++ run.py
@@ -5,11 +5,11 @@ from bot.tg_log import Logger
 print("installing packages from requirements.txt")
 os.system('pip install -r requirements.txt')
 print('done')
-import spam_classification
 if len(sys.argv) > 1:
     chat_id = sys.argv[1]
     sys.stdout = Logger(chat_id)
 else:
+    import spam_classification
     print("chat_id is required for tg logging")
 try:
     spam_classification.main()
