08ea9872b957f1a3d5de104eb713ea24b8903e75
Jonas Sitzmann <jonasitzmann@github.com>
2019-11-09 00:11:02 +0100

    adjust number of features to input dim

--- src/unit1_challenge2.py
+++ src/unit1_challenge2.py
@@ -84,7 +84,7 @@ class Task(BaseTask):
     def get_params(self):
         return {
             'feature_extraction__bag_of_words__ngram_range': (1, 3),
-            'feature_selection__k': 1000,
+            'feature_selection__k': 10000,  # todo: update net.input_dim accordingly
             #'classifier__hidden_layer_sizes': (10, 10)
         }
 
