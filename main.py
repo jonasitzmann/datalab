c1f3524a194902526fc4a85e805840a92fab9566
Jonas Sitzmann <jonasitzmann@github.com>
2019-11-17 14:34:08 +0100

    first attempt for unit 2 challenge 2 (PDF) result: 96.7% balanced acc (cv on all training data)

--- main.py
+++ main.py
@@ -14,7 +14,20 @@ def get_task(unit, challenge, samples_factor=1):
 
 
 if __name__ == '__main__':
-    task: BaseTask = get_task(unit=2, challenge=1, samples_factor=0.02)
+    # # experimental
+    # from tools.pdfid.pdfid import PDFiD
+    # from xml.dom.minidom import Document
+    # pdfid: Document = PDFiD('data/unit_2/challenge_2/train/1_malicious/zzaahzbrbfaetvhl_2017.pdf')
+    # count_dict = {}
+    # hexcount_dict = {}
+    # for node in pdfid.documentElement.getElementsByTagName('Keywords')[0].childNodes:
+    #     count_dict[node.getAttribute('Name')] = int(node.getAttribute('Count'))
+    #     hexcount_dict[node.getAttribute('Name')] = int(node.getAttribute('HexcodeCount'))
+    # print(count_dict)
+    # print(hexcount_dict)
+
+
+    task: BaseTask = get_task(unit=2, challenge=2, samples_factor=0.1)
     parallel = True  # set to False for debugging
     verbose = True
     # task.find_params(parallel=parallel, verbose=verbose)
--- src/unit2_challenge2.py
+++ src/unit2_challenge2.py
@@ -1,1 +1,60 @@
 # todo: take a look at https://zeltser.com/media/docs/analyzing-malicious-document-files.pdf
+from src.base.task import BaseTask
+from sklearn.feature_extraction.text import HashingVectorizer
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.decomposition import PCA
+from sklearn.svm import SVC
+from sklearn.feature_selection import SelectKBest, chi2
+from sklearn.pipeline import Pipeline
+from sklearn.base import TransformerMixin
+from src.utils.utils import DenseTransformer
+from tools.pdfid.pdfid import PDFiD
+from xml.dom.minidom import Document
+from io import BytesIO
+
+
+class PDFKeywordCounter(TransformerMixin):
+    def __init__(self):
+        super(PDFKeywordCounter, self).__init__()
+
+    def fit(self, x, y=None, **fit_params):
+        return self
+
+    def transform(self, x, y=None):
+        return [self.extract_keywords(pdf_file) for pdf_file in x]
+
+    def extract_keywords(self, pdf_file):
+        pdf_as_bytes = BytesIO(pdf_file)
+        pdfid: Document = PDFiD(pdf_as_bytes, file_is_binary=True)
+        count_dict = {}
+        hexcount_dict = {}
+        for node in pdfid.documentElement.getElementsByTagName('Keywords')[0].childNodes:
+            count_dict[node.getAttribute('Name')] = int(node.getAttribute('Count'))
+            hexcount_dict[node.getAttribute('Name')] = int(node.getAttribute('HexcodeCount'))
+        return list(count_dict.values())
+
+
+class Task(BaseTask):
+    @property
+    def include_file_names(self):
+        return False
+
+    @property
+    def unit(self):
+        return 2
+
+    @property
+    def challenge(self):
+        return 2
+
+    def get_model(self):
+        return Pipeline([
+            ('keyword_counter', PDFKeywordCounter()),
+            ('svm', SVC(gamma='auto')),
+        ])
+
+    def get_param_distribution(self):
+        return {}
+
+    def get_params(self):
+        return {}
